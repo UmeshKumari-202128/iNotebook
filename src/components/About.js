0bc284a143841952eab1a8e76bc723853d761dcb
Umesh kumari <umeshkumari123029@gmail.com>
2023-10-17 14:11:23 +0530

    Initial Commit

---
+++ src/components/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+
+      <h2 className="card-title mx-3 my-3">About iNotebook</h2>
+      <div className="card">
+        <div className="card-body">
+          
+          <p className="card-text">
+            iNotebook is a user-friendly application designed to streamline
+            note-taking and organization. It offers users the ability to create,
+            edit, and delete personalized notes, making it a versatile tool for
+            managing digital content. To ensure the privacy and security of
+            users' notes, the app incorporates a robust login and signup system.
+            New users can easily create an account through the signup feature,
+            granting them access to their own personal space for note storage.
+            With this comprehensive set of features, iNotebook provides a
+            seamless and secure platform for users to manage and access their
+            notes efficiently.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
